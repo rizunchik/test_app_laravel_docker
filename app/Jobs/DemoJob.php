0b684f76ff48a06d473eed2495285be555a3a650
rizunchik <rizunchik@gmail.com>
2025-10-03 18:54:35 +0300

    setting queue

---
+++ app/Jobs/DemoJob.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
+
+class DemoJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Log::info('DemoJob started');
+        usleep(500000); 
+        Log::info('DemoJob finished');
+    }
+}
