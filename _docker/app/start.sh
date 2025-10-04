35bb5b507ba74fc501bcfe2625d34b9dc573995f
rizunchik <rizunchik@gmail.com>
2025-10-04 13:52:24 +0300

    change docker configuration

---
+++ _docker/app/start.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+composer install --no-interaction --prefer-dist --optimize-autoloader 
+
+php artisan storage:link
+
+if [ ! -f /var/www/.env ]; then
+  cp /var/www/.env.example /var/www/.env
+fi
+
+php -r "
+        \$e = file_get_contents('.env');
+        if (!preg_match(\"/^APP_KEY=\\S+/m\", \$e)) { exit(1); }
+      " || php artisan key:generate --force
+
+echo "Waiting for database..."
+until php artisan migrate --force; do
+  echo "Retrying migrations in 5s..."
+  sleep 5
+done
+
+exec php-fpm
